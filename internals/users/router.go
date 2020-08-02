5fa846ec6ab21e960935e9923109e16685df0a5e
Al-un <alun.sng@gmail.com>
2020-08-02 10:56:06 +0900

    feat: temporary allow all users to access the users list

--- internals/users/router.go
+++ internals/users/router.go
@@ -9,7 +9,9 @@ import (
 
 func LoadEndpoints(router *mux.Router) {
 	router.HandleFunc("/users/login/", handleLogin).Methods(http.MethodPost, http.MethodOptions)
-	router.Handle("/users/", core.DoIfAdmin(handleListUsers)).Methods(http.MethodGet, http.MethodOptions)
+	// Temporary allow all logged users to fetch the user list. This has to be removed when the username
+	// will be attached to the review response
+	router.Handle("/users/", core.DoIfLogged(handleListUsers)).Methods(http.MethodGet, http.MethodOptions)
 	router.Handle("/users/", core.DoIfAdmin(handleCreateUser)).Methods(http.MethodPost, http.MethodOptions)
 	router.Handle("/users/{userID}/", core.DoIfLogged(handleGetUser)).Methods(http.MethodGet, http.MethodOptions)
 	router.Handle("/users/{userID}/", core.DoIfAdmin(handleUpdateUser)).Methods(http.MethodPut, http.MethodOptions)
